2bce883c1b3af66ec769a27eddc427c55c380068
HtetWaiYanAung74 <htetwyag75@gmail.com>
2024-09-09 11:46:21 +0700

    first commit

---
+++ tests/home.spec.ts
@@ -0,0 +1,31 @@
+import { test, expect } from '@playwright/test';
+import exp from 'constants';
+
+test.describe('Home page', () => {
+    test('Verify url and logo ', async ({ page }) => {
+        // access the page
+        await page.goto('https://www.icc-cricket.com/tournaments/cricketworldcup');
+
+        // verify url
+         expect(page.url()).toContain('cricket');
+        
+         // check logo visible
+          expect(page.locator("//img[@alt='Header Logo']")).toBeVisible;
+    })
+
+    test('Scenario 2, Search and verify new url and heading', async ({ page }) => {
+        // access the page
+        await page.goto('https://www.icc-cricket.com/tournaments/cricketworldcup');
+
+        // click search
+        await page.locator("//span[@class='font-cta inline']").click();
+        
+
+        const searchInput = page.getByPlaceholder('what are you looking for?');
+        await searchInput.fill('India');
+        await searchInput.press('Enter');
+
+      
+    })   
+    
+})
